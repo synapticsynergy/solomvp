11f45ede2ec3ea29edefe5b3bd8ec81434285ec7
Blake Danson <blakedanson@gmail.com>
2016-07-11 12:55:03 -0700

    deployment start

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//Server logger
+app.use(morgan('dev'));
+
+//Parses a the request to be handles as a plain object
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Serves up your public/client folder
+app.use(express.static('index.html'));
+
+//Sample routing
+app.route('/')
+  .get(function(req, res){
+    res.send("index.html will come soon!");
+  })
+  .post()
+  .put()
+  .delete();
+
+var port = process.env.PORT || 8000;
+
+app.listen(8000, function(){
+  console.log('server is now listening on port 8000.');
+});
