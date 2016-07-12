cc177dac279ce939e3b683791355b1d57f40a750
Blake Danson <blakedanson@gmail.com>
2016-07-12 16:49:29 -0700

    added ng-show for message and picture

--- public/app/calcController.js
+++ public/app/calcController.js
@@ -4,6 +4,15 @@ angular.module('equityCalc')
 .controller('calcController', function($scope) {
   $scope.roi = 0;
 
+  $scope.isPositive = function() {
+    if ($scope.roi > 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  };
+
   $scope.calcRoe = function() {
     $scope.roi = ($scope.netIncome * 12) / ($scope.capRate * 0.01);
   };
