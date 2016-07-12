8a1e078d866f0668d711e2aeece4a08cc9717fc4
Blake Danson <blakedanson@gmail.com>
2016-07-11 19:07:54 -0700

    cleaned up code, added app directory

--- public/app.js
+++ public/app.js
@@ -1,14 +1,4 @@
 angular.module('equityCalc', [
 
-])
-// .config(function ($routeProvider, $httpProvider) {
-//   $routeProvider
-//     .when('/signin', {
-//       templateUrl: 'app/auth/signin.html',
-//       controller: 'AuthController'
-//     })
-//     .otherwise({
-//       redirectTo: '/'
-//     });
+]);
 
-// })
